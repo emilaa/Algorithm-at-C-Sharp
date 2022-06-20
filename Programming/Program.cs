545bddd0b8260580011eb8dee764ab4cbd0daee2
emilaa <emilaa@code.edu.az>
2022-06-20 14:31:11 +0400

    New add 'cs.' file

---
+++ Programming/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Whether the number N is divisible by 3 and 7.
+
+            //int n = 21;
+
+            //if (n % 3 == 0 && n % 7 == 0)
+            //{
+            //    Console.WriteLine("divided");
+            //}
+            //else
+            //    Console.WriteLine("not divided");
+
+            //2.If the numbers N and M are both pairs, the sum of the numbers N and M.
+
+            //int n = 2;
+            //int m = 4;
+            //int sum = 0;
+
+            //if (n % 2 == 0 && m % 2 == 0)
+            //{
+            //    Console.WriteLine(sum = n + m);
+            //}
+
+            //3.The sum of odd numbers between N and M.
+
+            //int n = 1;
+            //int m = 10;
+            //int sum = 0;
+
+            //for (int i = n; i < m; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        sum = sum + i;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //4.The sum of the odd numbers in the Array.
+
+            //int[] number = { 2, 5, 6, 8, 11, 15 };
+            //int sum = 0;
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] % 2 == 1)
+            //    {
+            //        sum += number[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //5.The count of even numbers in the Array.
+
+            //int[] number = { 3, 6, 5, 8, 10, 9 };
+            //int count = 0;
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] % 2 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+        }
+    }
+}
